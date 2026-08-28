1becf1f8eee3532b416f8807586a746c300c20b5
Bhuvanesh-techhub <bhuvaneshb.cs@gmail.com>
2026-08-28 16:29:18 +0530

    Add session 1 loop and break programs

---
+++ src/main/java/combining loops and breaks/assignment_problems/Sum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Sum {
+
+    static void sumOfNaturalNumbers(int n) {
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        sumOfNaturalNumbers(n);
+    }
+}
